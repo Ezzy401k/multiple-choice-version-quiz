28c8c0ed317ca97f2c7a2b41f5cd0b9d7bd3fc4e
Esrael Mekdem <esraelmekdem@gmail.com>
2024-03-10 15:31:27 +0300

    Update question_data.py

--- question_data.py
+++ question_data.py
@@ -1,6 +1,13 @@
 class Questions:
     def __init__(self, question, options, answer):
-        self.question = question
-        self.option = options
-        self.answer = answer
+        """
+        Initializes a Questions object with the provided question, options, and answer.
 
+        Args:
+        question (str): The text of the question.
+        options (list): List of options for the question.
+        answer (str): The correct answer to the question.
+        """
+        self.question = question  # Stores the question text
+        self.options = options    # Stores the list of options
+        self.answer = answer      # Stores the correct answer
